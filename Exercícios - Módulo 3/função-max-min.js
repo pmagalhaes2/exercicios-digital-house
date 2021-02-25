03cbeda6112f38a264a95f2d3a094d4a27d94599
Patricia Magalhães <paath.magalhaes2@gmail.com>
2021-02-25 16:18:54 -0300

    Commit

---
+++ Exercícios - Módulo 3/função-max-min.js
@@ -0,0 +1,11 @@
+/*
+
+Escreva uma função maxmin(a, b, c, d, e) que receba cinco números como parâmetros.
+Sua função deve retornar um array onde o primeiro elemento é o menor dos parâmetros e o segundo elemento é o maior dos parâmetros.
+
+*/
+
+function maxmin(a, b, c, d, e){
+    return Math.min(a, b, c, d, e)
+    return Math.max((a, b, c, d, e))
+}
---
+++ Exercícios - Módulo 3/função-menor-número.js
@@ -0,0 +1,21 @@
+/*
+
+Escreva uma função chamada menorNumero. 
+
+Ela deve receber dois números como parâmetro e retornar o menor entre eles. 
+
+Caso os números sejam iguais, basta que se retorne qualquer um deles.
+
+*/
+
+function menorNumero(a, b){
+    if(a < b){
+        return a
+    }
+    if(b < a){
+        return b
+    }
+    else
+        return a
+    }
+    
