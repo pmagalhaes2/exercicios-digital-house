c0a46af88d566239a21ceaf6a8fb46a53102fd9c
Patricia Magalhães <paath.magalhaes2@gmail.com>
2021-02-25 15:54:32 -0300

    Commit

---
+++ Exercícios - Módulo 3/bom-dia.js
@@ -0,0 +1,11 @@
+/*
+
+Vamos criar um robô que manda “Bom dia, grupo!” para cada dia da semana, ou seja,  7 vezes. 
+
+Utilize o laço de repetição for para fazer com mais praticidade!
+
+*/
+
+for(var dia = 1; dia <= 7; dia++) {
+    console.log("Bom dia, grupo!")
+}
