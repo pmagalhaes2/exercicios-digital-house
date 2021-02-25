ead5883d1f3ed61607939f7753df1f7d2d8663df
Patricia Magalhães <paath.magalhaes2@gmail.com>
2021-02-25 16:10:01 -0300

    Commit

---
+++ Exercícios - Módulo 3/função-cartão-visita.js
@@ -0,0 +1,9 @@
+/*
+
+Crie uma função chamada cartaoDeVisitas, ela deverá imprimir o seu nome em conjunto com sobrenome! 
+
+*/
+
+function cartaoDeVisitas(){
+    console.log("Patricia" + " Magalhães")
+}
