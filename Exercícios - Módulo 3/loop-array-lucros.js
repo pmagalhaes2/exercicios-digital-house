7fa0e052d644c3a9b077ecc77aa92f4526a010e3
Patricia Magalhães <paath.magalhaes2@gmail.com>
2021-02-25 15:58:28 -0300

    Commit

---
+++ Exercícios - Módulo 3/loop-array-lucros.js
@@ -0,0 +1,18 @@
+/*
+
+Uma empresa separou em uma lista, os valores dos lucros mensais. Com isso você terá no código um array com o seguinte nome: listaDeLucro contendo em cada posição o valor de recebido de cada mês!
+
+var listaDeLucro = [100, 30, 300, -10, 600, 10]
+
+
+Seu trabalho será criar um loop que calcule o valor total baseado nessa lista, e coloque o valor em uma variável já existente no código chamada: lucroTotal
+
+*/
+
+var listaDeLucro = [100, 30, 300, -10, 600, 10];
+var lucroTotal = 0;
+
+for(var i = 0; i < listaDeLucro.length; i++) {
+lucroTotal = listaDeLucro[i] + lucroTotal
+}
+console.log(lucroTotal);
