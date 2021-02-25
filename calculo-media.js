bd7b6cbfcac42c68080002c4f07aeaeec05c0c8b
Patricia Magalhães <paath.magalhaes2@gmail.com>
2021-02-24 22:01:29 -0300

    Resolução exercício

---
+++ calculo-media.js
@@ -0,0 +1,7 @@
+var trabalhoDeHistoria = 8.0
+var trabalhoDeMatematica = 7.0
+var trabalhoDeCiencia = 10
+var trabalhoDeGeografia = 9.5
+var totalDeTrabalhos = 4
+
+var media = (trabalhoDeHistoria + trabalhoDeMatematica + trabalhoDeCiencia + trabalhoDeGeografia)/totalDeTrabalhos;
